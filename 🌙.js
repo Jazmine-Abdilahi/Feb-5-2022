651fc291d85291c5563dca9093458762b74d9fe5
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2022-02-05 13:55:14 -0600

    Add files via upload

---
+++ 🌙.js
@@ -0,0 +1,30 @@
+// Sat Feb 5 video 50 - 60\9
+
+
+'use strict';
+
+const x = '11'
+if (x === 11) console.log('11');
+
+const calcAge = birthYear => 2037 = birthYear;
+console.log(calcAge(2010));
+
+///////////////////////////////////////////////////////////////////
+//// video 59 min 20:44
+const calc = tempAnplitude = function (temps) {
+
+    let max = temps[0];
+    let min = temps[0];
+    for(let i = 0; 1 < temps.length; i++) {
+        if (temps[i] > max) max = temps[i];
+        if (temps[i] < min);
+    }
+    console.log[max];
+};
+
+const calcTempAnplitudeNew = function {temp} {
+    let max = temps[0];
+    let min = temps[0];
+
+    for (let i)
+}
